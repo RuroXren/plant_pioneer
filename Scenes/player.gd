1e98b66818d6d77632f24ccc2a046da0b27e4668
RuroXren <cometa150808@gmail.com>
2025-03-01 10:46:18 +0900

    Added MOVE

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var tile_map = $"../TileMap"
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('ui_up'):
 		Move(Vector2.UP)
@@ -11,4 +13,13 @@ func _process(delta: float) -> void:
 		Move(Vector2.LEFT)
 
 func Move(direction: Vector2):
-	print(direction)
+	var current_tile: Vector2i = tile_map.local_to_map(global_position)
+	var target_tile: Vector2i = Vector2i(
+		current_tile.x + direction.x,
+		current_tile.y + direction.y,
+	)
+	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
+	if tile_data.get_custom_data("walkable") == false:
+		return
+	
+	global_position = tile_map.map_to_local(target_tile)
